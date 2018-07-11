58e45d8a54ad1b6f78a2fdd068233241aa640291
Palash Mondal <palashm@live.in>
2018-07-11 12:03:48 +0530

    Using async/await

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 /**
- * Take a callback and call it using the setTimeout function
+ * Here we use the await to pause the execution of the code
+ * until the Promise is resolved and call the callback passing in the time.
  * @param callback
  */
-const start = callback => {
-  setTimeout(() => {
-    callback('Hello');
-    setTimeout(() => {
-      callback('And Welcome');
-      setTimeout(() => {
-        callback('To Async Await Using TypeScript');
-      }, 1000);
-    }, 1000);
-  }, 1000);
+const startAsync = async callback => {
+  await sleep(1000);
+  callback('Hello');
+  await sleep(1000);
+  callback('And Welcome');
+  await sleep(1000);
+  callback('To Async Await Using TypeScript');
 };
+
+/**
+ * This function takes a number of milliseconds and returns a Promise that gets resolved
+ * using setTimeout after the given number of milliseconds.
+ * @param ms number of milliseconds
+ */
+const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 /**
  * Log the function result here
  */
-start(text => console.log(text));
+startAsync(text => console.log(text));
